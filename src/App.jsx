628b1fb839ee0c85305ab07f0b96c60288ba9d57
andrewhoncharenko <goncharenkoandrew5643@gmail.com>
2025-01-10 22:53:48 +0200

    readme

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,25 @@ import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 import TabButton from "./components/TabButton";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState();
   const items =  CORE_CONCEPTS.map(item => <CoreConcept key={item.title} {...item}/>);
+  let tabContent = <p>Please select a topic.</p>;
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
   }
   
+  if(selectedTopic) {
+    tabContent = <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+          </div>
+  }
   return (
     <div>
       <Header />
@@ -30,15 +42,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
